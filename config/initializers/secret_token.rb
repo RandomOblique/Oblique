0de422e540ffea585bd9e8772773a99434a14e09
Mark Anderson <oblique.github.com@ma7.org>
2011-10-03 16:04:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oblique::Application.config.secret_token = '106fdfc103790a90126437a4c44303914174073fca1c399ec68bfa750df1c9f4ca0d761bd82edd0e911b1109e5ef21caae8a93b72eaeea941810130fcdc3254a'
