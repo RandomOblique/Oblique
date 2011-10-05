21b5ef922a96952148512f6ab1d5440d4d4870ca
Mark Anderson <oblique.github.com@ma7.org>
2011-10-05 18:52:13 -0500

    cleared out scaffolding and added random

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Oblique::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +50,7 @@ Oblique::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => "strategies#random"
 
   # See how all your routes lay out with "rake routes"
 
