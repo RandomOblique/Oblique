85eb81f6b2dabfee06fcf288b7c21241f788264b
Mark Anderson <oblique.github.com@ma7.org>
2011-10-06 13:18:32 -0500

    remove taps mistake and force text/plain for output

--- app/controllers/strategies_controller.rb
+++ app/controllers/strategies_controller.rb
@@ -1,6 +1,7 @@
 class StrategiesController < ApplicationController
 
   def random
+    render :text => robots, :layout => false, :content_type => "text/plain"
     @strategy = Strategy.random
   end
 
